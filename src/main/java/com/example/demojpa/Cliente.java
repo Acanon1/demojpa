d5bb044b44b721940ef1f64f4031676383425bbd
anthonycanon <20180343@aloe.ulima.edu.pe>
2024-01-20 11:42:47 -0500

    commit cliente

--- src/main/java/com/example/demojpa/Cliente.java
+++ src/main/java/com/example/demojpa/Cliente.java
@@ -17,8 +17,10 @@ public class Cliente {
     private Long id;
     @Column(name = "cliente_nombre")
     private String nombre;
+    @Column(name = "cliente_apellido")
+    private String apellido;
     @Column(name = "cliente_email")
-    private String email;
+    private Long documento;
 
     
     public Long getId() {
@@ -33,12 +35,18 @@ public class Cliente {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public Long getDocumento() {
+        return documento;
+    }
+    public void setDocumento(Long documento) {
+        this.documento = documento;
+    }
     
 }
